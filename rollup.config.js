d649057a952205cecefbdf3166681d4fd15ac20c
Dawid Zbinski <dawid.zbinski@ringieraxelspringer.sk>
2019-06-09 11:41:57 +0200

    rollup config changes to enable versioning

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,9 @@
 import svelte from 'rollup-plugin-svelte';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
-import livereload from 'rollup-plugin-livereload';
+import html from 'rollup-plugin-bundle-html';
+import liveReload from 'rollup-plugin-livereload';
+import del from 'rollup-plugin-delete';
 import { terser } from 'rollup-plugin-terser';
 
 const production = !process.env.ROLLUP_WATCH;
@@ -9,19 +11,26 @@ const production = !process.env.ROLLUP_WATCH;
 export default {
 	input: 'src/main.js',
 	output: {
-		sourcemap: true,
+		sourcemap: !production,
 		format: 'iife',
 		name: 'app',
-		file: 'public/bundle.js'
+		file: 'public/bundle.[hash].js'
 	},
 	plugins: [
+	    production && html({
+            template: 'src/index.html',
+            dest: 'public',
+            filename: 'index.html',
+            inject: 'body',
+
+        }),
 		svelte({
 			// enable run-time checks when not in production
 			dev: !production,
 			// we'll extract any component CSS out into
 			// a separate file — better for performance
 			css: css => {
-				css.write('public/bundle.css');
+				css.write('public/bundle.[hash].css');
 			}
 		}),
 
@@ -35,11 +44,15 @@ export default {
 
 		// Watch the `public` directory and refresh the
 		// browser on changes when not in production
-		!production && livereload('public'),
+		!production && liveReload('public'),
 
 		// If we're building for production (npm run build
 		// instead of npm run dev), minify
-		production && terser()
+		production && terser(),
+
+        // Remove bundle.[hash].css.map when production
+        // (Svelte related d-_-b)
+        production && del({ targets: 'public/bundle.[hash].css.map', verbose: true })
 	],
 	watch: {
 		clearScreen: false
