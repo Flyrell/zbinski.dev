d1e6039142bc78c24851b219267d1d309195069d
Dawid Zbiński <dawid@zbinski.dev>
2019-09-21 11:19:09 +0200

    security fixes and cv added

--- src/App.svelte
+++ src/App.svelte
@@ -2,49 +2,55 @@
     const icons = [
         {
             src: 'icons/github-brands.svg',
-            alt: 'flyrell on Github',
+            alt: 'My Github profile',
             classname: 'icon--github',
             href: 'https://github.com/Flyrell'
         },
         {
             src: 'icons/npm-brands.svg',
-            alt: 'flyrell\'s packages on npm',
+            alt: 'My npm packages',
             className: 'icon--npm',
             href: 'https://www.npmjs.com/~flyrell'
         },
         {
             src: 'icons/linkedin-in-brands.svg',
-            alt: 'Dawid Zbiński on LinkedIn',
+            alt: 'My LinkedIn Profile',
             className: 'icon--linkedin',
             href: 'https://linkedin.com/in/dawidzbinski/'
         },
         {
             src: 'icons/facebook-brands.svg',
-            alt: 'Dawid Zbiński on Facebook',
+            alt: 'My Facebook Profile',
             className: 'icon--facebook',
             href: 'https://www.facebook.com/dawid.zbinski'
         },
+        {
+            src: 'icons/file-pdf.svg',
+            alt: 'My CV',
+            className: 'icon--cv',
+            href: 'assets/CV.pdf'
+        }
     ];
-    const getFullImageSrc = (src) => '/assets/images/' + src;
+    const getFullImageSrc = (src) => 'assets/images/' + src;
 
 </script>
 
 <style>
 .icons {
-    position: absolute;
     top: 50%;
     left: 50%;
-    transform: translate(-50%,-50%);
     display: flex;
-    justify-content: space-between;
+    position: absolute;
     align-items: center;
+    justify-content: space-between;
+    transform: translate(-50%,-50%);
 }
 .icon {
     width: 50px;
     height: 50px;
-    filter: drop-shadow(5px 5px 5px #1A1A1AAA);
     margin: 0 15px;
     transform: translateY(0);
+    filter: drop-shadow(5px 5px 5px #1A1A1AAA);
     transition: transform .15s ease-in-out, filter .15s ease-in;
 }
 .icon:hover {
